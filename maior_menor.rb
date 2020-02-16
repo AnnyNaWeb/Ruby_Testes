dda1d11b93ca0b68b6fa6369078d622e1ace1546
ElaGameDev <sousa.annyc@gmail.com>
2020-02-16 01:53:24 -0300

    refatorando função e variáveis

--- maior_menor.rb
+++ maior_menor.rb
@@ -6,13 +6,18 @@ def bemvindo
 	puts "Começaremos o jogo para você, " + nome + "\n"
 end
 
+def sorteia_num
+	puts "Escolhendo um número entre 0 e 200..."
+	sorteado = 53
+	puts "Número escolhido. Que tal adivinhar? \n\n"
+	
+	sorteado
+end
 bemvindo
-
-puts "Escolhendo um número entre 0 e 200..."
-num = 53
-puts "Número escolhido. Que tal adivinhar? \n\n"
+num = sorteia_num
 
 limite = 3
+	
 for tentativa in 1...limite
 	puts "Tentativa " + tentativa.to_s + " de " + limite.to_s
 	puts "Entre com um número: "
