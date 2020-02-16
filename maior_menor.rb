8a2131f62b8e7a56e8fe38a9923512b4dd0945a6
ElaGameDev <sousa.annyc@gmail.com>
2020-02-16 01:33:35 -0300

    Laço de repetição for

--- maior_menor.rb
+++ maior_menor.rb
@@ -8,19 +8,22 @@ puts "Escolhendo um número entre 0 e 200..."
 num = 53
 puts "Número escolhido. Que tal adivinhar? \n\n"
 
-puts "Tentativa 001"
-puts "Entre com um número: "
-chute = gets
-puts "Será que acertou? Você chutou " + chute
+for tentativa in 1...3
+	puts "Tentativa " + tentativa.to_s + " de 3"
+	puts "Entre com um número: "
+	chute = gets
+	puts "Será que acertou? Você chutou " + chute
 
-acertou = num == chute.to_i
-if acertou
-	puts "Acertou! "
-else
-	maior = num > chute.to_i
-	if maior
-	puts "O número é maior!"
+
+	acertou = num == chute.to_i
+	if acertou
+		puts "Acertou! "
 	else
-	puts "O número é menor!"
+		maior = num > chute.to_i
+		if maior
+		puts "O número é maior!"
+		else
+		puts "O número é menor!"
+		end
 	end
 end
