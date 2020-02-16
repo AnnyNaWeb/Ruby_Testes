afd785d56d23cf6d37bb9fe801f66522dbbdf69b
ElaGameDev <sousa.annyc@gmail.com>
2020-02-16 01:18:53 -0300

    condicionais

--- maior_menor.rb
+++ maior_menor.rb
@@ -6,10 +6,14 @@ puts "Começaremos o jogo para você, " + nome + "\n"
 
 puts "Escolhendo um número entre 0 e 200..."
 num = 53
-puts "Número escolhido. Que tal adivinhar? \nn"
+puts "Número escolhido. Que tal adivinhar? \n\n"
 
 puts "Tentativa 001"
 puts "Entre com um número: "
 chute = gets
 puts "Será que acertou? Você chutou " + chute
-puts chute.to_i == num 
+if num == chute.to_i
+	puts "Acertou! "
+else
+	puts "Errou!"
+end
