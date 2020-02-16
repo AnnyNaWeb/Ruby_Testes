b9f1a333915444ddf078bf1279e0abd997317487
ElaGameDev <sousa.annyc@gmail.com>
2020-02-16 00:57:51 -0300

    Atribuição de variáveis

--- maior_menor.rb
+++ maior_menor.rb
@@ -6,3 +6,13 @@ puts
 puts
 puts
 puts "Começaremos o jogo para você, " + nome
+puts
+puts "Escolhendo um número entre 0 e 200..."
+num = 53
+puts "Número escolhido. Que tal adivinhar?"
+puts 
+puts
+puts "Tentativa 001"
+puts "Entre com um número: "
+chute = gets
+puts "Será que acertou? Você chutou " + chute
