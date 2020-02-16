f06aba64df38e938c46bc662f0242350441cbb17
ElaGameDev <sousa.annyc@gmail.com>
2020-02-16 01:15:17 -0300

    quebra de linha

--- maior_menor.rb
+++ maior_menor.rb
@@ -1,17 +1,13 @@
 puts "Bem vindo ao jogo de adivinhação"
 puts"Qual é o seu nome?"
 nome = gets
-puts
-puts
-puts
-puts
-puts "Começaremos o jogo para você, " + nome
-puts
+puts "\n \n \n \n"
+puts "Começaremos o jogo para você, " + nome + "\n"
+
 puts "Escolhendo um número entre 0 e 200..."
 num = 53
-puts "Número escolhido. Que tal adivinhar?"
-puts 
-puts
+puts "Número escolhido. Que tal adivinhar? \nn"
+
 puts "Tentativa 001"
 puts "Entre com um número: "
 chute = gets
